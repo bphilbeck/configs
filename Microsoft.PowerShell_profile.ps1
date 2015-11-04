86b30d13055d224067bd940a5913d1224d95e089
James Morgan <j.a.mrgn@gmail.com>
2015-11-04 10:19:43 -0500

    Updated powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,51 +1,80 @@
 Write-Host "Running main powershell profile..."
-function Grepout($pattern)
-{
-    git checkout $(git branch | grep $pattern).Trim()
-}
-Set-Alias grout Grepout
-Set-Alias open ii
+
 $projects = 'C:\projects'
-$framework = Join-Path $projects -childpath "Apps-Control-Panel-Framework"
-$bill = Join-Path $projects -childpath "Apps-Billing"
-$usage = Join-Path $projects -childpath "Apps-Control-Panel-Usage"
-$cp2 = Join-Path $projects -childpath "cp2\SA3-COM"
-$sa3 = Join-Path $cp2 -childpath "sa3"
-$cp3 = Join-Path $projects -childpath "Apps-Control-Panel"
-$chef = Join-Path $projects -childpath "Apps-Control-Panel-Chef"
-$conf = Join-Path $projects -childpath "configs"
-$cloud = Join-Path $projects -childpath "cloud-office-consumers"
+$share = 'C:\share'
+$agent = Join-Path $projects -childpath "agent"
+
 
 function proj { Set-Location $projects }
-function cp2 { Set-Location $cp2 }
-function sa3 { Set-Location $sa3 }
-function cp3 { Set-Location $cp3 }
-function bill { Set-Location $bill }
-function billing {Set-Location $bill }
-function framework { Set-Location $framework }
-function frame { Set-Location $framework }
-function usage { Set-Location $usage }
-function chef { Set-Location $chef }
-function conf { Set-Location $conf }
-function cloud { Set-Location $cloud }
+function agent { Set-Location $agent }
+function share { Set-Location $share }
+
 function rem($pattern)
 {
      get-childitem . -include $pattern -recurse | foreach ($_) {remove-item $_.fullname}
 }
 
-function touch 
+function touch
 {
 	set-content -Path ($args[0]) -Value ($null)
 }
 
 function Set-Home
 {
-	$h = "C:\Users\james.morgan\"
+	$h = "C:\Users\jamrg_000\"
 	Remove-Variable -Force HOME
 	Set-Variable HOME $h
 	$provider = get-psprovider filesystem
 	$provider.Home = $h
 }
 
+# Load posh-git example profile
+function Setup-PoshGit
+{
+	. C:\Users\jamrg_000\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1
+}
+
+function Greppable-File($file)
+{
+    return !($_.PSIsContainer) -and !($_.Name.ToString().ToLower().EndsWith(".dll")) -and !($_.Name.ToString().ToLower().EndsWith(".exe"))
+}
+
+function Grep-Current-Folder($pattern)
+{
+    Grep-Folder . $pattern
+}
+
+function Grep-Folder($folder, $pattern)
+{
+    gci $folder -rec | Where-Object { Greppable-File($_) } | foreach { select-string -path $_.FullName -pattern $pattern } 
+}
+
+# git-cl <LAST_TAG>..<MASTER|NEW_TAG>
+function GitChangeLog($rangeSpecification)
+{
+	git log --merges --grep="pull request" --pretty=format:'%C(yellow)%h%Creset - %s%n  %an %Cgreen(%cr)%C(bold blue)%d%Creset%n' $rangeSpecification
+}
+
+Set-Alias git-cl GitChangeLog
+
+function touch 
+{
+	set-content -Path ($args[0]) -Value ($null)
+}
+
+function Grepout($pattern)
+{
+    git checkout $(git branch | grep $pattern).Trim()
+}
+Set-Alias grout Grepout
+Set-Alias open ii
+Set-Alias git-cl GitChangeLog
+Set-Alias gcf Grep-Current-Folder
+Set-Alias gf Grep-Folder
+
 Set-Home
+Setup-PoshGit
+
+cd ~
+
 Write-Host "Profile complete."
